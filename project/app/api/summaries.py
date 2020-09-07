33d2e2f5b246ce637cc17d1f9eda5d4026504c0b
skredenmathias <skredenmathias@users.noreply.github.com>
2020-09-07 17:12:53 +0200

    Added parametrizing test functions. Ran Black & isort

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -2,7 +2,11 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema, SummaryUpdatePayloadSchema
+from app.models.pydantic import (
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 from app.models.tortoise import SummarySchema
 
 
@@ -41,8 +45,11 @@ async def delete_summary(id: int = Path(..., gt=0)) -> SummaryResponseSchema:
 
     return summary
 
+
 @router.put("/{id}/", response_model=SummarySchema)
-async def update_summary(payload: SummaryUpdatePayloadSchema, id: int = Path(..., gt=0)) -> SummarySchema:
+async def update_summary(
+    payload: SummaryUpdatePayloadSchema, id: int = Path(..., gt=0)
+) -> SummarySchema:
     summary = await crud.put(id, payload)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
