5896184d43e9658f4ffc653d92fe8abeb5f194d8
skredenmathias <skredenmathias@users.noreply.github.com>
2020-09-11 12:36:58 +0200

    implementing web scraping + summarizing with newspaper3k & nltk

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -2,6 +2,7 @@ from typing import Union, List
 
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+from app.summarizer import generate_summary
 
 
 # Utility function that creates new summaries
@@ -9,7 +10,8 @@ from app.models.tortoise import TextSummary
 # 2. Creates a TextSummary instance
 # 3. Returns the generated ID
 async def post(payload: SummaryPayloadSchema) -> int:
-    summary = TextSummary(url=payload.url, summary="dummy summary",)
+    article_summary = generate_summary(payload.url)
+    summary = TextSummary(url=payload.url, summary=article_summary)
     await summary.save()
     return summary.id
 
